afdc7ecad96a36a19f6743eee4f7644356d64ba2
Max <73070911+Maxth9@users.noreply.github.com>
2021-02-22 11:29:51 +0530

    Complete

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Test struct {
+	string string `json:"string`
+	integer int `json:"integer"`
+	bool bool `json:"bool"`
+}
+
+type Tests []Test
+
+func TestingFunc(w http.ResponseWriter, r *http.Request){
+	tester := Tests{
+		Test{string: "This is a string", integer: 1, bool: true},
+	}
+	fmt.Println("EndPoint Hit!")
+	json.NewEncoder(w).Encode(tester)
+}
 
 func landingPage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Endpoint Hit!")
@@ -12,6 +29,7 @@ func landingPage(w http.ResponseWriter, r *http.Request){
 
 func handleRequest(){
 	http.HandleFunc("/", landingPage)
+	http.HandleFunc("/test", TestingFunc)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
